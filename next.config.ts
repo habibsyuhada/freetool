b6398101ee2f0aa9e0bc92a1d1baed184d50de80
Habib Syuhada <habibsumedang@gmail.com>
2025-01-29 16:22:27 +0700

    remove console.log for prod

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+	compiler: {
+    removeConsole: true,
+  },
 };
 
 export default nextConfig;
